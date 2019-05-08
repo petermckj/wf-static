27afbd128a4492c5592b5dfe156220a2fad0d2bf
Peter McKenzie-Jones <petermckenziejones@gmail.com>
2019-05-08 14:10:58 +0100

    add spring animation

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,9 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import {Spring} from 'react-spring/renderprops'
+
+//const props = useSpring({opacity: 1, from: {opacity: 0},background: `rebeccapurple`,marginBottom: `1.45rem`})
 
 const Header = ({ siteTitle }) => (
   <header
@@ -16,17 +19,24 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <Spring
+        from={{ opacity: 0 }}
+        to={{ opacity: 1 }}>
+        {props => (
+          <h1 style={props}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+          </h1>
+        )}
+      </Spring>
+      
     </div>
   </header>
 )
